52d7f63cf05105f1c8b2ad375de2017f825c0327
jazminrodriguez1234 <136344528+jazminrodriguez1234@users.noreply.github.com>
2023-09-15 08:52:19 -0300

    Add files via upload
    examen de Programación

---
+++ examen1.js
@@ -0,0 +1,23 @@
+function salarioCalcular (b)
+{
+   let salarioAnual = 21600
+   let calcular = salarioAnual 
+    return calcular
+}
+console.log(salarioCalcular)
+//        //          //          //         //          //
+function salarioMensual (a)
+{
+    let salarioMensual = 1500
+    let calcularMensualmente = salarioMensual * a 
+    return calcularMensualmente
+}
+//        //         //          //         //           //
+nombreAlumno = prompt ('Ingresa tu Nombre')
+alert ('Bienvenido ' + nombreAlumno)
+function calcularPromedio (a,b,c)
+{
+    let promedio = a + b + c / 3
+    return promedio  
+}
+alert (calcularPromedio (6,3,2))
